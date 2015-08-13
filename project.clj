478d54f99afb857cdbce583cfbecfef7b3481ce6
Bill Tozier <bill@vagueinnovation.com>
2015-08-13 07:18:03 -0400

    updated dependencies to clojure 1.7.0 & midje 1.7.0

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject req "0.0.1-SNAPSHOT"
   :description "Interpreter for the ReQ language for genetic programming"
-  :dependencies [[org.clojure/clojure "1.4.0"]]
-  :profiles {:dev {:dependencies [[midje "1.5.1"]]}})
+  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :profiles {:dev {:dependencies [[midje "1.7.0"]]}})
   
