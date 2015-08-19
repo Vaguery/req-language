768d87d85f2ef9242789a6109bf99b080f2485f0
Bill Tozier <bill@vagueinnovation.com>
2015-08-19 09:30:55 -0400

    worked through Qlosure matching in core_test.clj

--- test/req/core_test.clj
+++ test/req/core_test.clj
@@ -84,7 +84,94 @@
 ;; in-place closure design
 ;; this is a literate design spike, so it's just going to be alternating tests and code here
 
-(fact "I can talk about numbers in Clojure"
-  (class 10) => java.lang.Long
-  (instance? Number 10/3) => true
+(defrecord Qlosure [wants result])
+
+(defn wants?
+  "determines whether one ReQ item wants another; that is
+  whether the first can use the second as an argument to a function"
+  [actor target]
+  (let [wants (:wants actor)]
+  (if wants
+    (some? (some #((second %) target) wants))
+    false
+    )))
+
+
+(fact "We can tell when any item wants another"
+  (wants? 3 4) => false
+  (wants? 3 false) => false
+  (wants? [1 2] [false :g]) => false
+  (wants? (Qlosure. {:arg1 integer?} 8) 4) => true
+  (wants? (Qlosure. {:arg1 float? :arg2 float?} 8) 4) => false
+  (wants? (Qlosure. {:arg1 some? :arg2 float?} 8) 4) => true
+  (wants? (Qlosure. {:arg1 some? :arg2 some?} 8) 4) => true
+  (wants? (Qlosure. {:arg1 float? :arg2 float?} 8) 4.2) => true
+  (wants? (Qlosure. {:arg1 float? :arg2 float?} 8) 4.2) => true
   )
+
+
+
+(defn can-interact?
+  "determines whether either of two ReQ items wants the other"
+  [a b]
+  (or (wants? a b) (wants? b a)))
+
+(fact "We can tell when either of two items wants the other"
+  (can-interact? 3 4) => false
+  (can-interact? 3 false) => false
+  (can-interact? [1 2] [false :g]) => false
+  (can-interact? (Qlosure. {:arg1 integer?} 8) 4) => true
+  (can-interact? 4 (Qlosure. {:arg1 integer?} 8)) => true
+  )
+
+(defn all-interactions
+  "walks through a collection and returns all things that
+  either are wanted by or want the actor"
+  [actor items]
+    (filter #(can-interact? actor %) items)
+    )
+
+(fact "We can tell all the items which an actor wants"
+  (let [inter (Qlosure. {:arg1 integer?} 8)]
+    (all-interactions 3 [1 2 3 4 5 6]) => []
+    (all-interactions inter [1 nil -11 3.2 4]) => [1 -11 4]
+    (all-interactions (Qlosure. {:arg1 some? :arg2 float?} 8) [1 [nil] -11 [false] 4]) => 
+      [1 [nil] -11 [false] 4]
+    (all-interactions (Qlosure. {:arg1 float? :arg2 float?} 8) [1 [nil] -11 [false] 4]) => []
+    (all-interactions (Qlosure. {:arg1 float? :arg2 #(= 4 %)} 8) [1.2 [nil] -11 [false] 4]) =>
+      [1.2 4]
+    (all-interactions 3 [1 2 3 4 5 6 inter]) => [inter]
+    (all-interactions inter [1.2 nil -11 3.2 4]) => [-11 4]
+  ))
+
+
+;; we want this function because clojure.core/split-with works in a certain way
+(defn do-not-interact?
+  "determines whether either of two ReQ items wants the other"
+  [a b]
+  (not (can-interact? a b)))
+
+(defn split-with-interaction
+  "splits a collection and returns a vector of two parts:
+  the first contains only things that do not interact with the
+  actor, the second begins with the first item that does interact
+  with the actor; if none of them interact with the actor, the first
+  collection will be empty"
+  [actor items]
+  (split-with (partial do-not-interact? actor) items))
+
+
+(fact "We get two piles"
+  (let [inter (Qlosure. {:arg1 integer?} 8)]
+    (split-with-interaction 3 [1 2 3 4 5 6]) => ['(1 2 3 4 5 6) '()]
+    (split-with-interaction inter [1.2 3.4 5 67]) => ['(1.2 3.4) '(5 67)]
+    (split-with-interaction inter [1.2 3.4 false 67]) => ['(1.2 3.4 false) '(67)]
+    (split-with-interaction inter [1 2 3]) => ['() '(1 2 3)]
+    (split-with-interaction inter [1.2 3.4 5/6]) => ['(1.2 3.4 5/6) '()]
+
+    (split-with-interaction 1.2 [1.2 3.4 5 67 inter]) =>  [`(1.2 3.4 5 67 ~inter) '()]
+    (split-with-interaction 11 [1.2 3.4 5 67 inter]) =>  [`(1.2 3.4 5 67) `(~inter)]
+    (split-with-interaction 11 [inter inter]) =>  [`() `(~inter ~inter)]
+  ))
+
+
