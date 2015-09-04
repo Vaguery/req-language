3364e81832056be95101ed39db530308eb2e541e
Bill Tozier <bill@vagueinnovation.com>
2015-09-04 15:56:59 -0400

    checked Imperatives can use set-channel

--- test/req/collections_test.clj
+++ test/req/collections_test.clj
@@ -11,60 +11,51 @@
 (fact "when req-dup is executed, two copies of a popped item are conj'ed onto the collection"
   (req-dup '(1 2 3 4)) => '(1 1 2 3 4)
   (req-dup [1 2 3 4]) => [1 2 3 4 4]
-  (req-dup (new-queue [1 2 3 4])) => [2 3 4 1 1]
-  )
+  (req-dup (new-queue [1 2 3 4])) => [2 3 4 1 1])
 
 
 (fact "req-dup returns the collection if it's empty"
   (req-dup '()) => '()
   (req-dup []) => []
-  (req-dup (new-queue)) => []
-)
+  (req-dup (new-queue)) => [])
 
 
 (fact "req-dup works on Interpreter instances"
-  (:queue (req-dup (make-interpreter [1 2 3 4]))) => [2 3 4 1 1]
-  )
+  (:queue (req-dup (make-interpreter [1 2 3 4]))) => [2 3 4 1 1])
 
 
 (fact "when req-pop is executed, a list holding a popped item and the remainder of the collection is returned"
   (req-pop '(1 2 3 4)) => '( 1 (2 3 4))
   (req-pop [1 2 3 4]) => '(4 [1 2 3])
   (req-pop (new-queue [1 2 3 4])) => '(1 (2 3 4))
-  (class (second (req-pop (new-queue [1 2 3 4])))) => clojure.lang.PersistentQueue
-  )
+  (class (second (req-pop (new-queue [1 2 3 4])))) => clojure.lang.PersistentQueue)
 
 
 (fact "req-pop returns the collection if it's empty"
   (req-pop '()) => '()
   (req-pop []) => []
-  (req-pop (new-queue)) => []
-)
+  (req-pop (new-queue)) => [])
 
 
 (fact "req-pop works on Interpreter instances, but discards the popped item"
-  (:queue (req-pop (make-interpreter [1 2 3 4]))) => [2 3 4]
-  )
+  (:queue (req-pop (make-interpreter [1 2 3 4]))) => [2 3 4])
 
 
 (fact "when req-archive is executed, the entire collection is conj'ed onto itself"
   (req-archive '(1 2 3 4)) => '((1 2 3 4) 1 2 3 4)
   (req-archive [1 2 3 4]) => '[1 2 3 4 [1 2 3 4]]
   (req-archive (new-queue [1 2 3 4])) => '(1 2 3 4 (1 2 3 4))
-  (class (last (req-archive (new-queue [1 2 3 4])))) => clojure.lang.PersistentQueue
-  )
+  (class (last (req-archive (new-queue [1 2 3 4])))) => clojure.lang.PersistentQueue)
 
 
 (fact "req-archive puts an empty copy into the collection even if it was empty"
   (req-archive '()) => '(())
   (req-archive []) => [[]]
-  (req-archive (new-queue)) => [[]]
-)
+  (req-archive (new-queue)) => [[]])
 
 
 (fact "req-archive works on Interpreter instances"
-  (:queue (req-archive (make-interpreter [1 2 3 4]))) => [1 2 3 4 [1 2 3 4]]
-  )
+  (:queue (req-archive (make-interpreter [1 2 3 4]))) => [1 2 3 4 [1 2 3 4]])
 
 
 
@@ -72,20 +63,17 @@
   (req-empty '(1 2 3 4)) => '()
   (req-empty [1 2 3 4]) => []
   (req-empty (new-queue [1 2 3 4])) => '()
-  (class (req-empty (new-queue [1 2 3 4]))) => clojure.lang.PersistentQueue
-  )
+  (class (req-empty (new-queue [1 2 3 4]))) => clojure.lang.PersistentQueue)
 
 
 (fact "req-empty works on empty collections, too"
   (req-empty '()) => '()
   (req-empty []) => []
-  (req-empty (new-queue)) => []
-)
+  (req-empty (new-queue)) => [])
 
 
 (fact "req-empty works on Interpreter instances"
-  (:queue (req-empty (make-interpreter [1 2 3 4]))) => []
-  )
+  (:queue (req-empty (make-interpreter [1 2 3 4]))) => [])
 
 
 (fact "when req-next is executed, the left item moves to the right end"
@@ -97,20 +85,17 @@
 
   (req-next (new-queue [1 2 3 4])) => [2 3 4 1]
   (queue? (new-queue [1 2 3 4])) => true
-  (queue? (req-next (new-queue [1 2 3 4]))) => true
-  )
+  (queue? (req-next (new-queue [1 2 3 4]))) => true)
 
 
 (fact "req-next returns the collection if it's empty"
   (req-next '()) => '()
   (req-next []) => []
-  (req-next (new-queue)) => []
-)
+  (req-next (new-queue)) => [])
 
 
 (fact "req-next works on Interpreter instances"
-  (:queue (req-next (make-interpreter [1 2 3 4]))) => [2 3 4 1]
-  )
+  (:queue (req-next (make-interpreter [1 2 3 4]))) => [2 3 4 1])
 
 
 (fact "when req-prev is executed, the rightmost item moves to the left end"
@@ -122,20 +107,17 @@
   
   (req-prev (new-queue [1 2 3 4])) => [4 1 2 3]
   (queue? (new-queue [1 2 3 4])) => true
-  (queue? (req-prev (new-queue [1 2 3 4]))) => true
-  )
+  (queue? (req-prev (new-queue [1 2 3 4]))) => true)
 
 
 (fact "req-prev returns the collection if it's empty"
   (req-prev '()) => '()
   (req-prev []) => []
-  (req-prev (new-queue)) => []
-)
+  (req-prev (new-queue)) => [])
 
 
 (fact "req-prev works on Interpreter instances"
-  (:queue (req-prev (make-interpreter [1 2 3 4]))) => [4 1 2 3]
-  )
+  (:queue (req-prev (make-interpreter [1 2 3 4]))) => [4 1 2 3])
 
 
 
@@ -148,20 +130,17 @@
   
   (req-reverse (new-queue [1 2 3 4])) => [4 3 2 1]
   (queue? (new-queue [1 2 3 4])) => true
-  (queue? (req-reverse (new-queue [1 2 3 4]))) => true
-  )
+  (queue? (req-reverse (new-queue [1 2 3 4]))) => true)
 
 
 (fact "req-reverse returns the collection if it's empty"
   (req-reverse '()) => '()
   (req-reverse []) => []
-  (req-reverse (new-queue)) => []
-)
+  (req-reverse (new-queue)) => [])
 
 
 (fact "req-reverse works on Interpreter instances"
-  (:queue (req-reverse (make-interpreter [1 2 3 4]))) => [4 3 2 1]
-  )
+  (:queue (req-reverse (make-interpreter [1 2 3 4]))) => [4 3 2 1])
 
 
 (fact "when req-swap is executed, the collection is flipped end-to-end"
@@ -173,17 +152,14 @@
   
   (req-swap (new-queue [1 2 3 4])) => [3 4 1 2]
   (queue? (new-queue [1 2 3 4])) => true
-  (queue? (req-swap (new-queue [1 2 3 4]))) => true
-  )
+  (queue? (req-swap (new-queue [1 2 3 4]))) => true)
 
 
 (fact "req-swap returns the collection if it's empty"
   (req-swap '()) => '()
   (req-swap []) => []
-  (req-swap (new-queue)) => []
-)
+  (req-swap (new-queue)) => [])
 
 
 (fact "req-swap works on Interpreter instances"
-  (:queue (req-swap (make-interpreter [1 2 3 4]))) => [3 4 1 2]
-  )
+  (:queue (req-swap (make-interpreter [1 2 3 4]))) => [3 4 1 2])
--- test/req/imperative_test.clj
+++ test/req/imperative_test.clj
@@ -28,5 +28,13 @@
     (let [do-not-do-this (->Imperative "+" (fn [x] (+ x)))
         r (make-interpreter [do-not-do-this 1 2 3 4 5])]
     (readable-queue r) => ["‡+‡" "1" "2" "3" "4" "5"]
-    (:queue (step r)) => (throws Exception))
-  )
+    (:queue (step r)) => (throws Exception)))
+
+
+(fact "an Imperative can be used to set a Channel value"
+  (let [setter (->Imperative "setter" #(set-channel % "y" 88))
+        r (make-interpreter [setter 2])]
+    (readable-queue r) => ["‡setter‡" "2"]
+    (:queue (step r)) => [2]
+    (:channels (step r)) => {"y" 88}
+    ))
